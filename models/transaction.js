a177f3566f0a9fcc751766a25c087daf888e1ab1
Emily Pozzi <epozzi5@yahoo.com>
2021-07-09 21:07:22 -0500

    working with tutor finish

---
+++ models/transaction.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const theSchema = mongoose.Schema;
+
+const schemaTrans = new theSchema({
+  name: {
+    type: String,
+    trim: true,
+    required: "Enter your transaction name."
+  },
+  value: {
+    type: Number,
+    required: "Enter your amount with a number. "
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const theTransaction = mongoose.model("theTransaction", schemaTrans);
+
+module.exports = theTransaction;
